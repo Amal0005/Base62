424d25270f17dc5d6af4625a4661a3a1434b8c0d
Amal0005 <amalachu441345@gmail.com>
2026-07-07 20:10:55 +0530

    feat: initialize NestJS server with CORS and validation pipe configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
